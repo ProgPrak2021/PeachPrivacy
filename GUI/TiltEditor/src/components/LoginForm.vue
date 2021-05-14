1bea901ed02b988a661d21b730f529087f28897a
Samy Hamdad <you@example.com>
2021-05-14 15:19:19 +0200

    login()

--- GUI/TiltEditor/src/components/LoginForm.vue
+++ GUI/TiltEditor/src/components/LoginForm.vue
@@ -3,7 +3,7 @@
     <b-item v-b-modal.modal-1 size="sm" >Anmelden</b-item>
     <b-modal id="modal-1" title="Anmelden" @ok="login">
             <b-form-group id="ctrtUsername" label="Benutzername *">
-                <b-form-input v-model="username" placeholder="Bitte Benutzername eingeben." trim aria-required ></b-form-input>
+                <b-form-input v-model="username" placeholder="Bitte Benutzername eingeben." trim aria-required=""></b-form-input>
             </b-form-group>
             <b-form-group id="ctrlPasswort" label="Passwort *" v-bind:description="descriptionPasswort" aria-required>
                 <b-form-input v-model="passwort" placeholder="Bitte Passwort eingeben (10 Zeichen)." trim aria-required ></b-form-input>
@@ -13,6 +13,10 @@
 </template>
 
 <script>
+
+
+import axios from 'axios';
+
 export default {
   name: "LoginModal",
   data() {
@@ -32,6 +36,15 @@ export default {
 },
   methods: {
     login() {
+        axios.post('http://localhost:8081/api/auth/login', {
+            email: this.username,
+            password: this.password,
+        }).then(response =>{
+           console.log(response.data);
+
+        }).catch(function (error) {
+            console.log(error);
+        })
        console.log("Username= " + this.username + "Passwort= " + this.passwort);
     },
   },
