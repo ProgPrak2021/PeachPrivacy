ca098d1c0447469bc833e845f3a5c769caa5a658
IPat <ThisIsIPat@users.noreply.github.com>
2021-05-16 03:39:09 +0200

    Added JSON validator dependency

--- templateservice/build.gradle.kts
+++ templateservice/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.worldtuner.medeia:medeia-validator-jackson:1.1.0")  // "(only jackson-core is needed)"
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
