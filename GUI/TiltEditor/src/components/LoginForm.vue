3df040283459cd46b02f8f3ac7f70176b768b249
p4skal <paskalpaesler23@gmail.com>
2021-05-17 00:40:58 +0200

    Rename login field in frontend, add better validation to login / register form in backend

--- GUI/TiltEditor/src/components/LoginForm.vue
+++ GUI/TiltEditor/src/components/LoginForm.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <b-modal id="modal-1" title="Anmelden" @ok="login">
-            <b-form-group id="ctrtUsername" label="Benutzername *">
-                <b-form-input v-model="username" placeholder="Bitte Benutzername eingeben." trim aria-required=""></b-form-input>
+            <b-form-group id="ctrtEmail" label="E-Mail *">
+                <b-form-input v-model="email" placeholder="Bitte E-Mail eingeben." trim aria-required=""></b-form-input>
             </b-form-group>
             <b-form-group id="ctrlPasswort" label="Passwort *" v-bind:description="descriptionPasswort" aria-required>
                 <b-form-input type = "password" v-model="passwort" placeholder="Bitte Passwort eingeben (10 Zeichen)." trim aria-required ></b-form-input>
@@ -20,7 +20,7 @@ export default {
   name: "LoginModal",
   data() {
     return {
-      username: "",
+      email: "",
       passwort: "",
     };
   },
@@ -35,18 +35,14 @@ export default {
 },
   methods: {
     login() {
-        console.log("Samy: Username = " + this.username + " Passwort = " + this.passwort);
-       //Login Aufruf für an den Server  
         axios.post('/api/auth/login', {
-            email: this.username,
+            email: this.email,
             password: this.password,
-        }).then(response =>{
+        }).then(response => {
            console.log(response.data);
-
         }).catch(function (error) {
             console.log(error);
         })
-       console.log("Username= " + this.username + "Passwort= " + this.passwort);
     },
   },
 };
--- userservice/src/main/kotlin/com/peachprivacy/userservice/authentication/LoginForm.kt
+++ userservice/src/main/kotlin/com/peachprivacy/userservice/authentication/LoginForm.kt
@@ -1,3 +1,10 @@
 package com.peachprivacy.userservice.authentication
 
-data class LoginForm(var email: String, var password: String)
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+data class LoginForm(
+    @Email @NotBlank @Size(max = 255) var email: String,
+    @NotBlank @Size(min = 6, max = 255) var password: String
+)
