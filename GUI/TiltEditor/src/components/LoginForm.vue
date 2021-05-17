5b96083adaff277e2bd433b8e1fceba27c7ef6c6
p4skal <paskalpaesler23@gmail.com>
2021-05-17 02:42:32 +0200

    Change json form data to request parameters

--- GUI/TiltEditor/src/components/LoginForm.vue
+++ GUI/TiltEditor/src/components/LoginForm.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <b-modal id="modal-login" title="Anmelden" hide-header-close @ok="login">
-            <b-form-group id="ctrtEmail" label="E-Mail *">
-                <b-form-input type="email" v-model="user.email" placeholder="Bitte Email als Benutzername eingeben." trim aria-required=""></b-form-input>
-            </b-form-group>
-            <b-form-group id="ctrlPasswort" label="Passwort *" v-bind:description="descriptionPasswort" aria-required>
-                <b-form-input type = "password" v-model="user.password" placeholder="Bitte Passwort eingeben (Min. 10 Zeichen)." trim aria-required ></b-form-input>
-            </b-form-group>
+      <b-form-group id="ctrtEmail" label="E-Mail *">
+        <b-form-input type="email" v-model="user.email" placeholder="Bitte Email als Benutzername eingeben." trim
+                      aria-required=""></b-form-input>
+      </b-form-group>
+      <b-form-group id="ctrlPasswort" label="Passwort *" v-bind:description="descriptionPasswort" aria-required>
+        <b-form-input type="password" v-model="user.password" placeholder="Bitte Passwort eingeben (Min. 10 Zeichen)."
+                      trim aria-required></b-form-input>
+      </b-form-group>
     </b-modal>
   </div>
 </template>
@@ -19,32 +21,37 @@ export default {
   name: "LoginModal",
   data() {
     return {
-      user:{
+      user: {
         email: "",
         password: ""
       }
     };
   },
   computed: {
-             descriptionPasswort() {
-                var len=0; 
-                len = 10 - this.user.password.length
-                return 'Noch ' + len.toString() + ' Zeichen übrig';
-            }
+    descriptionPasswort() {
+      var len = 0;
+      len = 10 - this.user.password.length
+      return 'Noch ' + len.toString() + ' Zeichen übrig';
+    }
 
   },
   methods: {
     login() {
-       //Login Aufruf für an den Server
-      console.log("Email= " + this.user.email + " Passwort= " + this.user.password);
+      //Login Aufruf für an den Server
+      console.log("Email = " + this.user.email + " Passwort = " + this.user.password);
 
-        axios.post('/api/auth/login', this.user).then(response => {
-           console.log(response.data);
-        }).catch(function (error) {
-            console.log(error);
-        })
-       this.$emit('login-success', this.user);
-       console.log("Email= " + this.user.email + " Passwort= " + this.user.password);
+      axios.post('/api/auth/login', null, {
+        params: {
+          email: this.user.email,
+          password: this.user.password
+        }
+      }).then(response => {
+        console.log(response.data);
+      }).catch(function (error) {
+        console.log(error);
+      })
+      this.$emit('login-success', this.user);
+      console.log("Email = " + this.user.email + " Passwort = " + this.user.password);
     },
   },
 };
--- GUI/TiltEditor/src/components/RegisterForm.vue
+++ GUI/TiltEditor/src/components/RegisterForm.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <b-modal id="modal-register" title="Register" hide-header-close @ok="register" >
-         <b-form-group id="ctrtEmail" label="E-Mail *">
-                <b-form-input type="email" v-model="user.email" placeholder="Bitte Email eingeben." trim aria-required=""></b-form-input>
-            </b-form-group>
-            <b-form-group id="Passwort" label="Passwort *" v-bind:description="descriptionPassword" aria-required>
-                <b-form-input type = "password" v-model="user.password" placeholder="Bitte Passwort eingeben (10 Zeichen)." trim aria-required ></b-form-input>
-            </b-form-group>
-             <b-form-group id="repeatPasswort" label="Passwort wiederholen*" aria-required>
-                <b-form-input type = "password" v-model="confirm_password" placeholder="Bitte Passwort wiederholen." trim aria-required ></b-form-input>
-            </b-form-group>
+    <b-modal id="modal-register" title="Register" hide-header-close @ok="register">
+      <b-form-group id="ctrtEmail" label="E-Mail *">
+        <b-form-input type="email" v-model="user.email" placeholder="Bitte Email eingeben." trim
+                      aria-required=""></b-form-input>
+      </b-form-group>
+      <b-form-group id="Passwort" label="Passwort *" v-bind:description="descriptionPassword" aria-required>
+        <b-form-input type="password" v-model="user.password" placeholder="Bitte Passwort eingeben (10 Zeichen)." trim
+                      aria-required></b-form-input>
+      </b-form-group>
+      <b-form-group id="repeatPasswort" label="Passwort wiederholen*" aria-required>
+        <b-form-input type="password" v-model="confirm_password" placeholder="Bitte Passwort wiederholen." trim
+                      aria-required></b-form-input>
+      </b-form-group>
 
     </b-modal>
   </div>
@@ -22,17 +25,17 @@ export default {
   name: "RegisterModal",
   data() {
     return {
-      user:{
+      user: {
         email: "",
         password: ""
       },
       confirm_password: "",
-      register_response:""
+      register_response: ""
     };
   },
   computed: {
     descriptionPassword() {
-      var len=0;
+      var len = 0;
       len = 10 - this.user.password.length
       return 'Noch ' + len.toString() + ' Zeichen übrig';
     }
@@ -41,16 +44,21 @@ export default {
   methods: {
     register() {
       //Login Aufruf für an den Server
-      console.log("Email= " + this.user.email + " Passwort= " + this.user.password);
+      console.log("Email = " + this.user.email + " Passwort = " + this.user.password);
 
-      axios.post('/api/auth/register', this.user).then(response =>{
-        this.register_response=response.data
+      axios.post('/api/auth/register', null, {
+        params: {
+          email: this.user.email,
+          password: this.user.password
+        }
+      }).then(response => {
+        this.register_response = response.data
       }).catch(function (error) {
         console.log(error);
       })
       this.$emit('register-success', this.user);
       console.log(this.register_response);
-      console.log("Email= " + this.user.email + " Passwort= " + this.user.password);
+      console.log("Email = " + this.user.email + " Passwort = " + this.user.password);
     },
   },
 };
