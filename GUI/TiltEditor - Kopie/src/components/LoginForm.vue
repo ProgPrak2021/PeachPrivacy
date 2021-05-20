1d54dc4588c0f106ef74618940eeb4db96902709
Vervada <lisa.a.barthel@campus.tu-berlin.de>
2021-05-20 14:41:20 +0200

    Registerstand

---
+++ GUI/TiltEditor - Kopie/src/components/LoginForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-modal id="modal-1" title="Anmelden" @ok="login">
+            <b-form-group id="ctrtUsername" label="Benutzername *">
+                <b-form-input v-model="username" placeholder="Bitte Benutzername eingeben." trim aria-required ></b-form-input>
+            </b-form-group>
+            <b-form-group id="ctrlPasswort" label="Passwort *" v-bind:description="descriptionPasswort" aria-required>
+                <b-form-input type = "password" v-model="passwort" placeholder="Bitte Passwort eingeben (10 Zeichen)." trim aria-required ></b-form-input>
+            </b-form-group>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginModal",
+  data() {
+    return {
+      username: "",
+      passwort: "",
+    };
+  },
+  computed: {
+             descriptionPasswort() {
+                var len=0; 
+                len = 10 - this.passwort.length
+                return 'Noch ' + len.toString() + ' Zeichen übrig';
+            }
+
+
+},
+  methods: {
+    login() {
+       console.log("Samy: Username = " + this.username + " Passwort = " + this.passwort);
+       //Hier muss der Aufruf für den Server rein 
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ GUI/TiltEditor - Kopie/src/components/TiltFormMeta.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="tilt-block">
+        <table>
+                    <tr>
+                        <td>
+                            <h3>Meta:</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Identifikationsnummer:</td>
+                        <td><input type="number" name="idNum" required autofocus></td> 
+                    </tr>
+                    <tr>
+                        <td>Name:</td>
+                        <td><input type="text" name="name" required></td>
+                    </tr>
+                    <tr>
+                        <td>Erstellungsdatum:</td>
+                        <td><input type="date" name="crDate" required></td>
+                    </tr>
+                    <tr>
+                        <td>Letzte Änderung:</td>
+                        <td><input type="date" name="modDate"></td>
+                    </tr>
+                    <tr>
+                        <td>Version:</td>
+                        <td><input type="number" name="version"></td>
+                    </tr>
+                    <tr>
+                        <td>Länderkennung:</td>
+                        <td><input type="text" name="langCode" ></td>
+                    </tr>
+                    <tr>
+                        <td>Status:</td>
+                        <td><input type="text" name="status" list="stati"></td>
+                        <datalist id="stati">
+                            <option value="fertiggestellt"></option>
+                            <option value="in Bearbeitung"></option>
+                        </datalist>
+                    </tr>
+                    <tr>
+                        <td>URL:</td>
+                        <td><input type="url" name="url"></td>
+                    </tr>
+                    <tr>
+                        <td>Hash:</td>
+                        <td><input type="text" name="hash"></td>
+                    </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TiltFormMeta',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
