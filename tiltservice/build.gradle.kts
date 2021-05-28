34107dab41297725f2a14736ff36df8d0361c339
IPat <ThisIsIPat@users.noreply.github.com>
2021-05-28 23:48:59 +0200

    Quickfix bug of cloud config local removal

--- tiltservice/build.gradle.kts
+++ tiltservice/build.gradle.kts
@@ -26,8 +26,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    // implementation("org.springframework.cloud:spring-cloud-starter-config")
-    // implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.h2database:h2")
