db3ff12715bb5d3f3272266bce51bf1157651079
IPat <ThisIsIPat@users.noreply.github.com>
2021-05-29 22:35:21 +0200

    add readiness probe controller

---
+++ tiltservice/src/main/kotlin/com/peachprivacy/tiltservice/ReadinessProbeController.kt
@@ -0,0 +1,12 @@
+package com.peachprivacy.tiltservice
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+// TODO: Readiness probes for ingress aren't relevant once gateway is configured properly
+@RestController
+class ReadinessProbeController {
+    @GetMapping
+    fun onReadinessProbeRequest() = ResponseEntity.ok()
+}
