daeb8f8859abe6d818e42207db7a1aa604f9944f
Samy Hamdad <you@example.com>
2021-06-26 17:47:45 +0200

    Datenschutzb.hover

--- GUI/TiltEditor/src/components/tiltforms/ProtectionOfficerForm.vue
+++ GUI/TiltEditor/src/components/tiltforms/ProtectionOfficerForm.vue
@@ -8,7 +8,7 @@
     <b-form id="modal-controller" title="Controller" >
 
       <b-form-group id="ctrladdress" label="Name">
-        <b-form-input  v-b-tooltip.hover :title="controllerInf.representative.name" :placeholder="[[controller.representative.name]]" type= datetime aria-required=""></b-form-input>
+        <b-form-input  v-b-tooltip.hover :title="controllerInf.name" :placeholder="[[controller.name]]" type= datetime aria-required=""></b-form-input>
       </b-form-group>
       <b-form-group id="ctrlcountry" label="Wohnort">
         <b-form-input  v-b-tooltip.hover :title="controllerInf.address" :placeholder="[[controller.address]]" aria-required=""></b-form-input>
@@ -17,10 +17,10 @@
         <b-form-input  v-b-tooltip.hover :title="controllerInf.country" :placeholder="[[controller.country]]" aria-required=""></b-form-input>
       </b-form-group>
       <b-form-group id="ctrlrepresentativeemail" label="E-Mail">
-        <b-form-input  v-b-tooltip.hover :title="controllerInf.representative.email" :placeholder="[[controller.representative.email]]" aria-required=""></b-form-input>
+        <b-form-input  v-b-tooltip.hover :title="controllerInf.email" :placeholder="[[controller.email]]" aria-required=""></b-form-input>
       </b-form-group>
       <b-form-group id="ctrlrepresentativephone" label="Telefonnummer">
-        <b-form-input  v-b-tooltip.hover :title="controllerInf.representative.phone" :placeholder="[[controller.representative.phone]]" aria-required=""></b-form-input>
+        <b-form-input  v-b-tooltip.hover :title="controllerInf.phone" :placeholder="[[controller.phone]]" aria-required=""></b-form-input>
       </b-form-group>
     </b-form>
   </div>
@@ -28,29 +28,27 @@
 
 <script>
 export default {
-  name: "controller",
+  name: "protectionOfficer",
   data() {
     return {
       controller: {
         address: "Wolfsburger Ring 2, 38440 Berlin",
         country: "DE",
-        representative: {
-          name: "Jane Super",
-          email: "contact@greencompany.de",
-          phone: "0049 151 1234 5678"
-        }
+        name: "Jane Super",
+        email: "contact@greencompany.de",
+        phone: "0049 151 1234 5678"
+
       },
       controllerInf: {
         address: "Hier tragen Sie bitte den Wohnort des Datenschutzbeauftragten ein",
         country: "Hier tragen Sie bitte das Land des Wohnortes ein.",
-        representative: {
-          name: " Geben Sie in dieses Feld den vollständigen Namen des Mitarbeiters ein, der für die Einhaltung der Datenschutzrichtlinien zuständig ist (Datenschutzbeauftragter).",
-          email: "Die Emailadresse dieses Mitarbeiters schreiben Sie bitte in dieses Feld.",
-          phone: "Tragen Sie bitte hier die Telefonnummer des Mitarbeiters ein"
-        }
+        name: " Geben Sie in dieses Feld den vollständigen Namen des Mitarbeiters ein, der für die Einhaltung der Datenschutzrichtlinien zuständig ist (Datenschutzbeauftragter).",
+        email: "Die Emailadresse dieses Mitarbeiters schreiben Sie bitte in dieses Feld.",
+        phone: "Tragen Sie bitte hier die Telefonnummer des Mitarbeiters ein"
       }
     }
   }
+
 }
 </script>
 
