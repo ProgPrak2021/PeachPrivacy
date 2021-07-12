5764a345dc7ff9048f3109285dc47d5519f31765
IPat <ThisIsIPat@users.noreply.github.com>
2021-07-12 03:34:48 +0200

    version updates

--- templateservice/build.gradle.kts
+++ templateservice/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.peachprivacy"
-version = "1.0.6"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
--- tiltservice/build.gradle.kts
+++ tiltservice/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.peachprivacy"
-version = "1.1.0"
+version = "1.1.2"
 
 repositories {
     mavenCentral()
